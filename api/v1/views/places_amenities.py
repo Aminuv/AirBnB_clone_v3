dfc1b0c23a8e447cf80dfa6cf8bc97b584749534
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-10-31 00:00:10 +0000

    Update places_amenities

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -14,7 +14,7 @@ from os import environ
 @app_views.route('places/<place_id>/amenities', methods=['GET'],
                  strict_slashes=False)
 def get_place_amenities(place_id):
-    """Retrieves the list of all Amenity objects of a place"""
+    """ Retrieves the list of all Amenity objects of a place"""
     place = storage.get("Place", place_id)
     if not place:
         abort(404)
@@ -30,7 +30,7 @@ def get_place_amenities(place_id):
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['DELETE'], strict_slashes=False)
 def delete_place_amenity(place_id, amenity_id):
-    """Deletes an Amenity object from a place"""
+    """ Deletes an Amenity object from a place"""
     place = storage.get('Place', place_id)
     if not place:
         abort(404)
@@ -54,7 +54,7 @@ def delete_place_amenity(place_id, amenity_id):
 @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'],
                  strict_slashes=False)
 def link_place_amenity(place_id, amenity_id):
-    """Links an amenity object to a place"""
+    """ Links an amenity object to a place"""
     place = storage.get('Place', place_id)
     if not place:
         abort(404)
